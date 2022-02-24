a950142af319019355764247b0a12e851f2e3760
Philip Colllier <redcrown01@gmail.com>
2022-02-24 02:47:44 +0000

    Update sdr-installer.sh

--- sdr-installer.sh
+++ sdr-installer.sh
@@ -11,6 +11,9 @@
 # To disable a specific installer, comment its reference in
 # the function list near the bottom of this script.
 
+# define working directory
+working_dir="/usr/local/src"
+
 # get the latest release from git repos
 download_last(){
     cd $3
@@ -26,19 +29,19 @@ printf "\n\n...SDRplay MiricsAPI..."
 target_file="SDRplay_RSP_API-Linux-3.07.1.run"
 #printf "\nGet it manually from https://www.sdrplay.com/dlfinishs/"
 #printf "\nRobots are people too."
-cd /usr/local/src
-[[ -f "/usr/local/src/$target_file" ]] \
+cd "$working_dir"
+[[ -f "$working_dir/$target_file" ]] \
     && chmod 755 "$target_file" \
     && ./"$target_file"
 }
 
 get_libmirisdr_4() {
 printf "\n\n...LibMiriSDR-4..."
-cd /usr/local/src
-[[ -f "/usr/local/src/libmirisdr-4" ]] \
-    || git clone https://github.com/f4exb/libmirisdr-4 \
-    && mkdir -p /usr/local/src/libmirisdr-4/build
-cd /usr/local/src/libmirisdr-4/build
+cd "$working_dir"
+[[ -f "$working_dir/libmirisdr-4" ]] \
+    || git clone "https://github.com/f4exb/libmirisdr-4" \
+    && mkdir -p $working_dir/libmirisdr-4/build
+cd $working_dir/libmirisdr-4/build
 cmake ..
 make -j4
 make install
@@ -46,11 +49,11 @@ make install
 
 get_SoapySDR_core() {
 printf "\n\n...SoapySDR..."
-cd /usr/local/src
-[[ -f "/usr/local/src/SoapySDR" ]] \
-    || git clone https://github.com/pothosware/SoapySDR \
-    && mkdir -p /usr/local/src/SoapySDR/build
-cd /usr/local/src/SoapySDR/build
+cd "$working_dir"
+[[ -f "$working_dir/SoapySDR" ]] \
+    || git clone "https://github.com/pothosware/SoapySDR" \
+    && mkdir -p "$working_dir/SoapySDR/build"
+cd "$working_dir/SoapySDR/build"
 cmake ..
 make -j4
 make install
@@ -58,11 +61,11 @@ make install
 
 get_rtl_sdr_drivers() {
 printf "\n\n...rtl-sdr firmware..."
-cd /usr/local/src
-[[ -f "/usr/local/src/librtlsdr" ]] \
-    || git clone https://github.com/steve-m/librtlsdr \
-    && mkdir -p /usr/local/src/librtlsdr/build
-cd /usr/local/src/librtlsdr/build
+cd "$working_dir"
+[[ -f "$working_dir/librtlsdr" ]] \
+    || git clone "https://github.com/steve-m/librtlsdr" \
+    && mkdir -p "$working_dir/librtlsdr/build"
+cd "$working_dir/librtlsdr/build"
 cmake ../ -DINSTALL_UDEV_RULES=ON
 make -j4
 make install
@@ -70,11 +73,11 @@ make install
 
 get_gr_osmosdr() {
 printf "\n\n...gr-osmosdr..."
-cd /usr/local/src
-[[ -f "/usr/local/src/gr-osmosdr" ]] \
-    || git clone git://git.osmocom.org/gr-osmosdr \
-    && mkdir -p /usr/local/src/gr-osmosdr/build
-cd /usr/local/src/gr-osmosdr/build
+cd "$working_dir"
+[[ -f "$working_dir/gr-osmosdr" ]] \
+    || git clone "git://git.osmocom.org/gr-osmosdr" \
+    && mkdir -p "$working_dir/gr-osmosdr/build"
+cd "$working_dir/gr-osmosdr/build"
 cmake ..
 make -j4
 make install
@@ -82,11 +85,11 @@ make install
 
 get_SoapyRTLSDR() {
 printf "\n\n...SoapyRTLSDR..."
-cd /usr/local/src
-[[ -f "/usr/local/src/SoapyRTLSDR" ]] \
-    || git clone https://github.com/pothosware/SoapyRTLSDR \
-    && mkdir -p /usr/local/src/SoapyRTLSDR/build
-cd /usr/local/src/SoapyRTLSDR/build
+cd "$working_dir"
+[[ -f "$working_dir/SoapyRTLSDR" ]] \
+    || git clone "https://github.com/pothosware/SoapyRTLSDR" \
+    && mkdir -p "$working_dir/SoapyRTLSDR/build"
+cd "$working_dir/SoapyRTLSDR/build"
 cmake ..
 make -j4
 make install
@@ -94,11 +97,11 @@ make install
 
 get_SoapyPlutoSDR() {
 printf "\n\n...ADALM-PlutoSDR"
-cd /usr/local/src
-[[ -f "/usr/local/src/SoapyPlutoSDR" ]] \
-    || git clone https://github.com/pothosware/SoapyPlutoSDR \
-    && mkdir -p /usr/local/src/SoapyPlutoSDR/build
-cd /usr/local/src/SoapyPlutoSDR/build
+cd "$working_dir"
+[[ -f "$working_dir/SoapyPlutoSDR" ]] \
+    || git clone "https://github.com/pothosware/SoapyPlutoSDR" \
+    && mkdir -p "$working_dir/SoapyPlutoSDR/build"
+cd "$working_dir/SoapyPlutoSDR/build"
 cmake ..
 make -j4
 make install
@@ -106,11 +109,11 @@ make install
 
 get_SoapyRedPitaya() {
 printf "\n\n...SoapyRedPitaya"
-cd /usr/local/src
-[[ -f "/usr/local/src/SoapyRedPitaya" ]] \
-    || git clone https://github.com/pothosware/SoapyRedPitaya \
-    && mkdir -p /usr/local/src/SoapyRedPitaya/build
-cd /usr/local/src/SoapyRedPitaya/build
+cd "$working_dir"
+[[ -f "$working_dir/SoapyRedPitaya" ]] \
+    || git clone "https://github.com/pothosware/SoapyRedPitaya" \
+    && mkdir -p "$working_dir/SoapyRedPitaya/build"
+cd "$working_dir/SoapyRedPitaya/build"
 cmake ..
 make -j4
 make install
@@ -118,11 +121,11 @@ make install
 
 get_SoapySDRPlay3() {
 printf "\n\n...SoapySDRPlay3..."
-cd /usr/local/src
-[[ -f "/usr/local/src/SoapySDRPlay3" ]] \
-    || git clone https://github.com/pothosware/SoapySDRPlay3 \
-    && mkdir -p /usr/local/src/SoapySDRPlay3/build
-cd /usr/local/src/SoapySDRPlay3/build
+cd "$working_dir"
+[[ -f "$working_dir/SoapySDRPlay3" ]] \
+    || git clone "https://github.com/pothosware/SoapySDRPlay3" \
+    && mkdir -p "$working_dir/SoapySDRPlay3/build"
+cd "$working_dir/SoapySDRPlay3/build"
 cmake ..
 make -j4
 make install
@@ -130,16 +133,16 @@ make install
 
 get_Airspy() {
 printf "\n\n...LibAirspy..."
-cd /usr/local/src
-[[ -f "/usr/local/src/airspyone_host-master" ]] \
-    || mkdir -p /usr/local/src/airspyone_host-master/build
+cd "$working_dir"
+[[ -f "$working_dir/airspyone_host-master" ]] \
+    || mkdir -p "$working_dir/airspyone_host-master/build"
 # get the archived source for linux
 target_file="master.zip"
-cd "/usr/local/src/airspyone_host-master"
-wget https://github.com/airspy/airspyone_host/archive/master.zip
+cd "$working_dir/airspyone_host-master"
+wget "https://github.com/airspy/airspyone_host/archive/master.zip"
 unzip master.zip
 rm master.zip
-cd "/usr/local/src/airspyone_host-master/build"
+cd "$working_dir/airspyone_host-master/build"
 cmake ../ -DINSTALL_UDEV_RULES=ON
 make -j4
 make install
@@ -147,11 +150,11 @@ make install
 
 get_SoapyAirspy() {
 printf "\n\n...SoapyAirspy..."
-cd /usr/local/src
-[[ -f "/usr/local/src/SoapyAirspy" ]] \
-    || git clone https://github.com/pothosware/SoapyAirspy \
-    && mkdir -p /usr/local/src/SoapyAirspy/build
-cd /usr/local/src/SoapyAirspy/build
+cd "$working_dir"
+[[ -f "$working_dir/SoapyAirspy" ]] \
+    || git clone "https://github.com/pothosware/SoapyAirspy" \
+    && mkdir -p "$working_dir/SoapyAirspy/build"
+cd "$working_dir/SoapyAirspy/build"
 cmake ..
 make -j4
 make install
@@ -159,11 +162,11 @@ make install
 
 get_SoapyAirspyHF() {
 printf "\n\n...SoapyAirspyHF..."
-cd /usr/local/src
-[[ -f "/usr/local/src/SoapyAirspyHF" ]] \
-    || git clone https://github.com/pothosware/SoapyAirspyHF \
-    && mkdir -p /usr/local/src/SoapyAirspyHF/build
-cd /usr/local/src/SoapyAirspyHF/build
+cd "$working_dir"
+[[ -f "$working_dir/SoapyAirspyHF" ]] \
+    || git clone "https://github.com/pothosware/SoapyAirspyHF" \
+    && mkdir -p "$working_dir/SoapyAirspyHF/build"
+cd "$working_dir/SoapyAirspyHF/build"
 cmake ..
 make -j4
 make install
@@ -171,11 +174,11 @@ make install
 
 get_SoapyAudio() {
 printf "\n\n...soapy audio"
-cd /usr/local/src
-[[ -f "/usr/local/src/SoapyAudio" ]] \
-    || git clone https://github.com/pothosware/SoapyAudio \
-    && mkdir -p /usr/local/src/SoapyAudio/build
-cd /usr/local/src/SoapyAudio/build
+cd "$working_dir"
+[[ -f "$working_dir/SoapyAudio" ]] \
+    || git clone "https://github.com/pothosware/SoapyAudio" \
+    && mkdir -p "$working_dir/SoapyAudio/build"
+cd "$working_dir/SoapyAudio/build"
 cmake ..
 make -j4
 make install
@@ -183,11 +186,11 @@ make install
 
 get_SoapyBladeRF() {
 printf "\n\n...SoapyBladeRF"
-cd /usr/local/src
-[[ -f "/usr/local/src/SoapyBladeRF" ]] \
-    || git clone https://github.com/pothosware/SoapyBladeRF \
-    && mkdir -p /usr/local/src/SoapyBladeRF/build
-cd /usr/local/src/SoapyBladeRF/build
+cd "$working_dir"
+[[ -f "$working_dir/SoapyBladeRF" ]] \
+    || git clone "https://github.com/pothosware/SoapyBladeRF" \
+    && mkdir -p "$working_dir/SoapyBladeRF/build"
+cd "$working_dir/SoapyBladeRF/build"
 cmake ..
 make -j4
 make install
@@ -195,11 +198,11 @@ make install
 
 get_SoapyFCDPP() {
 printf "\n\n...SoapyFCDPP"
-cd /usr/local/src
-[[ -f "/usr/local/src/SoapyFCDPP" ]] \
-    || git clone https://github.com/pothosware/SoapyFCDPP \
-    && mkdir -p /usr/local/src/SoapyFCDPP/build
-cd /usr/local/src/SoapyFCDPP/build
+cd "$working_dir"
+[[ -f "$working_dir/SoapyFCDPP" ]] \
+    || git clone "https://github.com/pothosware/SoapyFCDPP" \
+    && mkdir -p "$working_dir/SoapyFCDPP/build"
+cd "$working_dir/SoapyFCDPP/build"
 cmake ../
 make -j4
 make install
@@ -207,11 +210,11 @@ make install
 
 get_SoapyHackRF() {
 printf "\n\n...SoapyHackRF"
-cd /usr/local/src
-[[ -f "/usr/local/src/SoapyHackRF" ]] \
-    || git clone https://github.com/pothosware/SoapyHackRF \
-    && mkdir -p /usr/local/src/SoapyHackRF/build
-cd /usr/local/src/SoapyHackRF/build
+cd "$working_dir"
+[[ -f "$working_dir/SoapyHackRF" ]] \
+    || git clone "https://github.com/pothosware/SoapyHackRF" \
+    && mkdir -p "$working_dir/SoapyHackRF/build"
+cd "$working_dir/SoapyHackRF/build"
 cmake ..
 make -j4
 make install
@@ -219,11 +222,11 @@ make install
 
 get_SoapyLMS7() {
 printf "\n\n...SoapyLMS7"
-cd /usr/local/src
-[[ -f "/usr/local/src/LimeSuite" ]] \
-    || git clone https://github.com/myriadrf/LimeSuite \
-    && mkdir -p /usr/local/src/LimeSuite/build
-cd /usr/local/src/LimeSuite/build
+cd "$working_dir"
+[[ -f "$working_dir/LimeSuite" ]] \
+    || git clone "https://github.com/myriadrf/LimeSuite" \
+    && mkdir -p $working_dir/LimeSuite/build
+cd $working_dir/LimeSuite/build
 cmake ..
 make -j4
 make install
@@ -231,11 +234,11 @@ make install
 
 get_SoapyRadioberrySDR() {
 printf "\n\n...SoapyRadioberrySDR"
-cd /usr/local/src
-[[ -f "/usr/local/src/SBC/rpi-4/SoapyRadioberrySDR" ]] \
-    || git clone https://github.com/pa3gsb/Radioberry-2.x \
-    && mkdir -p /usr/local/src/Radioberry-2.x/SBC/rpi-4/SoapyRadioberrySDR/build
-cd /usr/local/src/Radioberry-2.x/SBC/rpi-4/SoapyRadioberrySDR/build
+cd "$working_dir"
+[[ -f "$working_dir/SBC/rpi-4/SoapyRadioberrySDR" ]] \
+    || git clone "https://github.com/pa3gsb/Radioberry-2.x" \
+    && mkdir -p "$working_dir/Radioberry-2.x/SBC/rpi-4/SoapyRadioberrySDR/build"
+cd "$working_dir/Radioberry-2.x/SBC/rpi-4/SoapyRadioberrySDR/build"
 cmake ..
 make -j4
 make install
@@ -243,11 +246,11 @@ make install
 
 get_SoapyUHD() {
 printf "\n\n...SoapyUHD"
-cd /usr/local/src
-[[ -f "/usr/local/src/SoapyUHD" ]] \
-    || git clone https://github.com/pothosware/SoapyUHD \
-    && mkdir -p /usr/local/src/SoapyUHD/build
-cd /usr/local/src/SoapyUHD/build
+cd "$working_dir"
+[[ -f "$working_dir/SoapyUHD" ]] \
+    || git clone "https://github.com/pothosware/SoapyUHD" \
+    && mkdir -p "$working_dir/SoapyUHD/build"
+cd "$working_dir/SoapyUHD/build"
 cmake ..
 make -j4
 make install
@@ -255,11 +258,11 @@ make install
 
 get_gqrx() {
 printf "\n\n...Gqrx"
-cd /usr/local/src
-[[ -f "/usr/local/src/gqrx" ]] \
-    || git clone https://github.com/csete/gqrx \
-    && mkdir -p /usr/local/src/gqrx/build
-cd /usr/local/src/gqrx/build
+cd "$working_dir"
+[[ -f "$working_dir/gqrx" ]] \
+    || git clone "https://github.com/csete/gqrx" \
+    && mkdir -p "$working_dir/gqrx/build"
+cd "$working_dir/gqrx/build"
 cd build
 cmake ..
 make -j4
@@ -283,18 +286,18 @@ Terminal=false
 Icon=gqrx
 Categories=HamRadio;
 Keywords=SDR;Radio;HAM;
-' > $HOME/.local/share/applications/gqrx.desktop
+' > "$HOME/.local/share/applications/gqrx.desktop"
 }
 
 get_SDRpp() {
 printf "\n\n...SDRplusplus"
-cd /usr/local/src
-[[ -f "/usr/local/src/SDRPlusPlus" ]] \
-    && mkdir -p /usr/local/src/SDRPlusPlus
+cd "$working_dir"
+[[ -f "$working_dir/SDRPlusPlus" ]] \
+    && mkdir -p $working_dir/SDRPlusPlus
 # download from github
 target_file="sdrpp_ubuntu_focal_amd64.deb"
 git_repo="AlexandreRouma/SDRPlusPlus"
-dl_dir="/usr/local/src/SDRPlusPlus"
+dl_dir="$working_dir/SDRPlusPlus"
 cd "$dl_dir"
 download_last $git_repo $target_file $dl_dir
 dpkg -i $target_file
