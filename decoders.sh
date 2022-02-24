bc317d331312eae8e22c2b1d6f2c6fac3ee582b8
Philip Colllier <redcrown01@gmail.com>
2022-02-24 21:29:24 +0000

    Update decoders.sh

--- decoders.sh
+++ decoders.sh
@@ -23,12 +23,12 @@ export -f download_last
 
 update_glrpt() {
 printf "\n\n...glrpt..."
-cd "$working_dir"
-apt install libturbojpeg libturbojpeg0-dev
-[[ -f "$working_dir/glrpt" ]] \
+cd "${working_dir}"
+apt install -y libturbojpeg libturbojpeg0-dev
+[[ -f "${working_dir}/glrpt" ]] \
     || git clone "https://github.com/dvdesolve/glrpt" \
-    && mkdir -p "$working_dir/glrpt/build"
-cd "$working_dir/glrpt/build"
+    && mkdir -p "${working_dir}/glrpt/build"
+cd "${working_dir}/glrpt/build"
 cmake -DCMAKE_INSTALL_PREFIX=/usr ..
 make
 make install
@@ -36,22 +36,22 @@ make install
 
 update_audioprism() {
 printf "\n\n...audioprism..."
-cd "$working_dir"
-apt install libpulse-dev libfftw3-dev libsdl2-dev libsdl2-ttf-dev \
+cd "${working_dir}"
+apt install -y libpulse-dev libfftw3-dev libsdl2-dev libsdl2-ttf-dev \
     libsndfile1-dev libgraphicsmagick++1-dev
-[[ -f "$working_dir/audioprism" ]] \
+[[ -f "${working_dir}/audioprism" ]] \
     || git clone "https://github.com/vsergeev/audioprism"
-cd $working_dir/audioprism
+cd "${working_dir}/audioprism"
 make
 make install
 }
 
 update_kalibrate-rtl() {
 printf "\n\n...kalibrate-rtl..."
-cd "$working_dir"
-[[ -f "$working_dir/kalibrate-rtl" ]] \
+cd "${working_dir}"
+[[ -f "${working_dir}/kalibrate-rtl" ]] \
     || git clone "https://github.com/steve-m/kalibrate-rtl"
-cd "$working_dir/kalibrate-rtl"
+cd "${working_dir}/kalibrate-rtl"
 ./bootstrap && CXXFLAGS='-W Wall -03'
 ./configure
 make
@@ -60,10 +60,10 @@ make install
 
 update_dump1090() {
 printf "\n\n...dump1090..."
-cd "$working_dir"
-[[ -f "$working_dir/dump1090-fa" ]] \
+cd "${working_dir}"
+[[ -f "${working_dir}/dump1090-fa" ]] \
     || git clone "https://github.com/adsbxchange/dump1090-fa"
-cd "$working_dir/dumpdump1090-fa"
+cd "${working_dir}/dumpdump1090-fa"
 make
 cp dump1090 /usr/local/bin/dump1090
 cp view1090 /usr/local/bin/view1090
@@ -71,11 +71,11 @@ cp view1090 /usr/local/bin/view1090
 
 update_RTLSDR-Airband() {
 printf "\n\n...RTLSDR-Airband..."
-cd "$working_dir"
-[[ -f "$working_dir/RTLSDR-Airband" ]] \
+cd "${working_dir}"
+[[ -f "${working_dir}/RTLSDR-Airband" ]] \
     || git clone "https://github.com/szpajder/RTLSDR-Airband" \
-    && mkdir -p "$working_dir/RTLSDR-Airband/build"
-cd "$working_dir/RTLSDR-Airband/build"
+    && mkdir -p "${working_dir}/RTLSDR-Airband/build"
+cd "${working_dir}/RTLSDR-Airband/build"
 cmake -NFM=ON -DMIRISDR=OFF ../
 make
 make install
@@ -83,11 +83,11 @@ make install
 
 update_libacars() {
 printf "\n\n...libacars..."
-cd "$working_dir"
-[[ -f "$working_dir/libacars" ]] \
+cd "${working_dir}"
+[[ -f "${working_dir}/libacars" ]] \
     || git clone "https://github.com/szpajder/libacars" \
-    && mkdir -p "$working_dir/libacars/build"
-cd "$working_dir/libacars/build"
+    && mkdir -p "${working_dir}/libacars/build"
+cd "${working_dir}/libacars/build"
 cmake ../
 make
 make install
@@ -95,11 +95,11 @@ make install
 
 update_acarsdec() {
 printf "\n\n...acarsdec..."
-cd "$working_dir"
-[[ -f "$working_dir/acarsdec" ]] \
+cd "${working_dir}"
+[[ -f "${working_dir}/acarsdec" ]] \
     || git clone "https://github.com/szpajder/acarsdec" \
-    && mkdir -p $working_dir/acarsdec/build
-cd "$working_dir/acarsdec/build"
+    && mkdir -p "${working_dir}/acarsdec/build"
+cd "${working_dir}/acarsdec/build"
 cmake ../ -Drtl=ON
 make
 make install
@@ -107,11 +107,11 @@ make install
 
 update_vdlm2dec() {
 printf "\n\n...vdlm2dec..."
-cd "$working_dir"
-[[ -f "$working_dir/vdlm2dec" ]] \
+cd "${working_dir}"
+[[ -f "${working_dir}/vdlm2dec" ]] \
     || git clone "https://github.com/TLeconte/vdlm2dec" \
-    && mkdir -p "$working_dir/vdlm2dec/build"
-cd "$working_dir/vdlm2dec/build"
+    && mkdir -p "${working_dir}/vdlm2dec/build"
+cd "${working_dir}/vdlm2dec/build"
 cmake .. -Drtl=ON
 make
 make install
@@ -119,20 +119,20 @@ make install
 
 update_acarsserv() {
 printf "\n\n...acarsserv..."
-cd "$working_dir"
-[[ -f "$working_dir/acarsserv" ]] \
+cd "${working_dir}"
+[[ -f "${working_dir}/acarsserv" ]] \
     || git clone "https://github.com/TLeconte/acarsserv"
-cd "$working_dir/acarsserv"
+cd "${working_dir}/acarsserv"
 make -f makefile
 cp acarsserv "/usr/local/sbin/acarsserv"
 }
 
 update_dumpvdl2() {
 printf "\n\n...dumpvdl2..."
-cd "$working_dir"
-[[ -f "$working_dir/dumpvdl2" ]] \
+cd "${working_dir}"
+[[ -f "${working_dir}/dumpvdl2" ]] \
     || git clone "https://github.com/szpajder/dumpvdl2" \
-    && mkdir -p "$working_dir/dumpvdl2/build"
+    && mkdir -p "${working_dir}/dumpvdl2/build"
 cd "usr/local/src/dumpvdl2/build"
 cmake ../
 make
@@ -141,21 +141,21 @@ make install
 
 update_rtl-ais() {
 printf "\n\n...rtl-ais..."
-cd "$working_dir"
-[[ -f "$working_dir/rtl-ais" ]] \
+cd "${working_dir}"
+[[ -f "${working_dir}/rtl-ais" ]] \
     || git clone "https://github.com/dgiardini/rtl-ais"
-cd "$working_dir/rtl-ais"
+cd "${working_dir}/rtl-ais"
 make
 make install
 }
 
 update_noaa-apt() {
 printf "\n\n...noaa-apt..."
-cd "$working_dir"
+cd "${working_dir}"
 git_repo="martinber/noaa-apt"
 target_file="amd64.deb"
-dl_dir="$working_dir/noaa-apt"
-cd "$dl_dir"
+dl_dir="${working_dir}/noaa-apt"
+cd "${dl_dir}"
 download_last $git_repo $target_file $dl_dir
 deb_file="$(ls | grep "$target_file")"
 dpkg -i $deb_file
@@ -165,11 +165,11 @@ rm ./*.deb
 
 update_sdrtrunk() {
 printf "\n\n...SDRTrunk..."
-cd "$working_dir"
+cd "${working_dir}"
 git_repo="DSheirer/sdrtrunk"
 target_file="linux-x86_64"
-dl_dir="$working_dir"
-cd "$dl_dir"
+dl_dir="${working_dir}"
+cd "${dl_dir}"
 ## currently - download manually
 download_last $git_repo $target_file $dl_dir
 extracted_dir="$(ls | grep "$target_file")"
